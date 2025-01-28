49388e8ad877c61e0efd5fba39253d3f11d283ac
V1niciusK <vskussunoki@gmail.com>
2025-01-27 21:07:58 -0300

    beginning the development of server prep functions

--- src/main.py
+++ src/main.py
@@ -1,9 +1,53 @@
-from textnode import TextNode, TextType
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
+
+## Utils
+
+def cleanDir(dirname: str) -> None:
+    """Will erase the entirety of a very specific directory, not before logging all files and backing it up.
+
+    Args:
+        dirname (str): _description_
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    raise NotImplementedError
+
+def copyFiles(fileList: list[str]) -> None:
+    """Copy files from static/ to public/ recursively
+
+    Args:
+        fileList (list[str]): _description_
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    
+    if len(fileList) == 1:
+        raise NotImplementedError
+    pass
+
+## Major functions
+
+def publicDirectoryPrep() -> None:
+    """Will call cleanDir() to unpopulate public/ before calling copyFiles() in preparation for server startup. In the interest of keeping this function clean I have severed out the functions that will deal with os calls. For now they reside inside this main functiion but will eventually be migrated to a utils.py file.
+
+    Raises:
+        NotImplementedError: _description_
+    """
+    raise NotImplementedError
+
 
 
 def main():
-    test_text_node = TextNode( "Lorem ipsum dolor sit amet", TextType.bold , "https://www.boot.dev"  )
-    print( test_text_node )
+    # Prepare for server startup
+    publicDirectoryPrep()
+    
+    # Process md files
+    
+    # Start static content server
+    
 
 
 
